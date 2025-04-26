ebb7e6e0a450d7a3043c2d32b38e6fa396b1f3c6
Franco1625 <u202219284@upc.edu.pe>
2025-04-25 19:39:59 -0500

    feat(mechanic): add model and service for vehicle

--- lib/seller/data/services/vehicle_service.dart
+++ lib/seller/data/services/vehicle_service.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:car2go_mobile_app/shared/constants/constant.dart';
-import 'package:car2go_mobile_app/mechanic/data/models/vehicle_model.dart';
+import 'package:car2go_mobile_app/seller/data/models/vehicle_model.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -24,6 +24,28 @@ class VehicleService {
     } else {
       print('Error al obtener vehículos: ${response.body}');
       throw Exception('Fallo al cargar vehículos');
+    }
+  }
+
+  static Future<Vehicle> fetchVehicleById(int id) async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token');
+
+    final url = Uri.parse(Constant.getEndpoint('vehicle/$id'));
+    final response = await http.get(
+      url,
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = jsonDecode(response.body);
+      return Vehicle.fromJson(data);
+    } else {
+      print('Error al obtener vehículo por ID: ${response.body}');
+      throw Exception('Fallo al cargar vehículo');
     }
   }
 }
